72471752e07dce60bd56e34c39c3c80d51ec17e6
Wionek <krystianjonca43@gmail.com>
2022-04-24 14:18:49 +0200

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import Menu from './navbar/Menu';
 
 const StyledFooter = styled.footer`
   background-color: ${({ theme }) => theme.navy};
@@ -9,10 +11,49 @@ const StyledFooter = styled.footer`
   align-items: center;
   padding: 2rem;
   text-align: center;
+
+  h3 {
+    font-weight: 400;
+    font-size: var(--fs-md);
+    letter-spacing: 2px;
+    padding: 1rem;
+  }
+
+  a {
+    color: ${({ theme }) => theme.light};
+    cursor: pointer;
+    transition: color 0.3s linear;
+
+    &:hover {
+      color: ${({ theme }) => theme.gold};
+    }
+  }
+
+  .footer-icon {
+    transition-duration: 0.2s;
+    margin: 1rem;
+
+    &:hover {
+      transform: scale(1.08);
+    }
+  }
 `;
 
 const Footer = () => {
-  return <StyledFooter id="footer"></StyledFooter>;
+  return (
+    <StyledFooter>
+      <div className="footer-container">
+        <h3>Menu</h3>
+        <Menu footer={true} />
+        <FaFacebook size={32} className="footer-icon" />
+        <FaInstagram size={32} className="footer-icon" />
+        <p>
+          Created & Designed by{' '}
+          <a href="https://github.com/KrystianJonca">Krystian</a>
+        </p>
+      </div>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
