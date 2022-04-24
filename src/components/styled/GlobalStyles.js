118d15fb3798e891b66da08f1035da74e9a16270
Wionek <krystianjonca43@gmail.com>
2022-04-24 11:43:54 +0200

    Added global styles

--- src/components/styled/GlobalStyles.js
+++ src/components/styled/GlobalStyles.js
@@ -1,5 +1,36 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyles = createGlobalStyle``;
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --fs-xl: 5rem;
+    --fs-md: 2rem;
+    --fs-sm: 1.2rem;
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    scroll-behavior: smooth;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.light};
+    color: ${({ theme }) => theme.navy};
+  }
+
+  a {
+    color: ${({ theme }) => theme.navy};
+    text-decoration: none;
+  }
+
+  p, a {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3, h4 {
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
 
 export default GlobalStyles;
